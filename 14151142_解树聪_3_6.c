34b9ff78b25f0065a03a32969ac92a24442bec4d
2015CLanguage <buaa_2015_c@163.com>
2015-03-24 14:49:14 +0800

    Create 14151142_解树聪_3_6.c

---
+++ 14151142_解树聪_3_6.c
@@ -0,0 +1,19 @@
+#include<stdio.h>       //编译预处理指令
+int main()              //定义主函数
+{                       //函数开始标志
+	char c1='C',c2='h',c3='i',c4='n',c5='a';//定义变量类型为字符，并赋值
+	c1=c1+4;                                //运算
+	c2=c2+4;
+	c3=c3+4;
+	c4=c4+4;
+	c5=c5+4;
+	printf("密码为");
+	putchar(c1);                            //putchar函数输出结果
+	putchar(c2);                            //putchar函数输出结果
+	putchar(c3);                            //putchar函数输出结果
+	putchar(c4);                            //putchar函数输出结果
+	putchar(c5);                            //putchar函数输出结果
+	putchar('\n');                          //换行
+	printf("密码为%c%c%c%c%c\n",c1,c2,c3,c4,c5);//printf函数输出结果
+	return 0;                               //返回函数值0
+}                       //函数结束标志
