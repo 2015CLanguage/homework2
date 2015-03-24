03440c17705d413cad295564fed09238539a09f5
2015CLanguage <buaa_2015_c@163.com>
2015-03-24 15:16:55 +0800

    Update 14151198_高威_3_3.c

--- 14151198_高威_3_3.c
+++ 14151198_高威_3_3.c
@@ -3,9 +3,9 @@
 
 int main()
 {
-	double d=300000,p=6000,r=0.01,m;
-	m=log(p/(p-d*r));
-	m=m/log(1+r);
-	printf("%.1f\n",m);
+	double d = 300000,p = 6000,r = 0.01,m;
+	m = log(p/ (p-d*r) );
+	m = m / log( 1+r );
+	printf("m = %.1f\n",m);
     return 0;
 }
