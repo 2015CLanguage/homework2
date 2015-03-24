667d30434a1d4f537733a51417f60457ba44a5da
2015CLanguage <buaa_2015_c@163.com>
2015-03-24 14:32:56 +0800

    Create 14151138_庄云霄_3_6.c

---
+++ 14151138_庄云霄_3_6.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{	char c1,c2,c3,c4,c5,b1,b2,b3,b4,b5; //声明各种变量
+	c1='C';
+	c2='h';
+	c3='i';
+	c4='n';
+	c5='a';
+	b1=c1+4;
+	b2=c2+4;							//将各个字符加密
+	b3=c3+4;
+	b4=c4+4;
+	b5=c5+4;
+	printf("用putchar函数输出加密后的字符串为：");
+	putchar(b1);
+	putchar(b2);
+	putchar(b3);               //用putchar函数输出加密后的字符串
+	putchar(b4);
+	putchar(b5);
+	putchar('\n');
+	printf("用printf函数输出加密后的字符串为：%c%c%c%c%c\n",b1,b2,b3,b4,b5);          
+	                        //用printf函数输出加密后的字符串
+	return 0;
+}
