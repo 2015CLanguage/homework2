cc2811b2e6e5bd423246925da68a613f1c176637
2015CLanguage <buaa_2015_c@163.com>
2015-03-24 20:16:37 +0800

    Create 14151195_张雨生_3_6.c

---
+++ 14151195_张雨生_3_6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{char c1='C',c2='h',c3='i',c4='n',c5='a';//赋初始值。
+ c1=c1+4; //定义算法。
+ c2=c2+4;
+ c3=c3+4;
+ c4=c4+4;
+ c5=c5+4;
+ printf("密码是%c%c%c%c%c\n",c1,c2,c3,c4,c5);//输出“密码是....”。
+ return 0;
+}//结束。
