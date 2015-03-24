fff8d1a471d1d521ba4b5953016110f390e83c55
2015CLanguage <buaa_2015_c@163.com>
2015-03-24 20:42:48 +0800

    Create 14151156_邹爽_3_6_.c

---
+++ 14151156_邹爽_3_6_.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	char w1='C',w2='h',w3='i',w4='n',w5='a';
+	w1=w1+4;
+	w2=w2+4;
+	w3=w3+4;
+	w4=w4+4;
+	w5=w5+4;
+	printf("密码为：%c%c%c%c%c\n",w1,w2,w3,w4,w5);
+	return 0;
+}
