d9b12c85dc4bd54a5e0eeb28de5b9c2d0391d664
2015CLanguage <buaa_2015_c@163.com>
2015-03-24 15:00:09 +0800

    Create 14151151_孙志鹏_3_6.c

---
+++ 14151151_孙志鹏_3_6.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(void)
+{
+	char c1='C'+4;
+	char c2='h'+4;
+	char c3='i'+4;
+	char c4='n'+4;
+	char c5='a'+4;
+  
+  printf("%c%c%c%c%c\n",c1,c2,c3,c4,c5);
+
+	putchar(c1);
+	putchar(c2);
+	putchar(c3);
+	putchar(c4);
+	putchar(c5);
+	putchar('\n');
+		
+	return 0;
+}
