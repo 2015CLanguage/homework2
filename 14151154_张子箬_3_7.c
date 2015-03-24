bfc2f5ed16659187a7a46a5517546289e4bb40a3
2015CLanguage <buaa_2015_c@163.com>
2015-03-24 22:43:11 +0800

    Create 14151154_张子箬_3_7.c

---
+++ 14151154_张子箬_3_7.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+
+#define PI 3.1415926
+
+int main()
+{
+	float r;
+	int h;
+	double c,s,a,v1,v2;
+	printf("请输入圆半径r,圆柱高h:");
+	scanf("%f,%d",&r,&h);
+	c=2*PI*r;
+	s=PI*r*r;
+	a=4*PI*r*r;
+	v1=4*PI*pow(r,3)/3;
+	v2=s*h;
+	printf("圆周长为：%5.2f\n",c);
+	printf("圆面积为：%5.2f\n",s);
+	printf("圆球表面积为：%5.2f\n",a);
+	printf("圆球体积为：%5.2f\n",v1);
+	printf("圆柱体积为：%5.2f\n",v2);
+	return 0;
+}
