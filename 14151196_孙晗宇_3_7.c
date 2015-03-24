10fd6dc4aec66ffa43b67634dbe32ba7b4812ac2
2015CLanguage <buaa_2015_c@163.com>
2015-03-24 14:53:49 +0800

    Create 14151196_孙晗宇_3_7.c

---
+++ 14151196_孙晗宇_3_7.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+int main()
+{
+	float r,h,l,s1,s2,v1,v2;
+	printf("请输入圆半径：");
+	scanf("r=%f",h=%f",&r,&h);
+	l=(float)(2*r*3.14);
+	s1=(float)(3.4*r*r);
+	s2=(float)(4*3.14*r*r);
+	v1=(float)((float)4/3*3.14*r*r*r);
+	v2=(float)(s1*h);
+	printf("%.2f\n%.2f\n%.2f\n%.2f\n%.2f\n",l,s1,s2,v1,v2);
+	return 0;
+}
