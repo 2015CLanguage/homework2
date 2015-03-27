43e8442b8a018d7a845b36af5fc8dbb5f4e021df
2015CLanguage <buaa_2015_c@163.com>
2015-03-27 22:24:02 +0800

    Create 14151179_刘子超_3_2.c

---
+++ 14151179_刘子超_3_2.c
@@ -0,0 +1,14 @@
+#include"stdio.h"
+#include<math.h>
+int main()
+{
+	double h=1000.0,b=0.0414,c=0.0468,d=0.054,e=0.0585,f=0.0072,p1,p2,p3,p4,p5,x,y;
+	p1=a*(5*e+1);
+	p2=a*(c*2+1)*(d*3+1);
+	p3=a*(d*3+1)*(c*2+1);
+	x=a*(b+1)
+	p4=pow(x,5);
+	y=a*(f/4+1)
+	p5=pow(y,20);
+    printf("p1=%lf\np2=%lf\np3=%lf\np4=%lf\np5=%lf\n",p1,p2,p3,p4,p5);
+}
