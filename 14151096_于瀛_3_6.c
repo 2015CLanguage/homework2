23b6a963c4a862c4cffb0ce82a2710e5067dd0df
2015CLanguage <buaa_2015_c@163.com>
2015-03-27 21:19:55 +0800

    Create 14151096_于瀛_3_6.c

---
+++ 14151096_于瀛_3_6.c
@@ -0,0 +1,19 @@
+#include<stdio.h> 
+int main(void) 
+{ 
+  char c1='C'+4;//给字符变量赋值并进行处理 
+  char c2='h'+4; 
+  char c3='i'+4; 
+  char c4='n'+4; 
+  char c5='a'+4; 
+  
+  printf("%c%c%c%c%c\n",c1,c2,c3,c4,c5);//使用printf进行输出 
+  putchar(c1);//使用putchar进行输出 
+	putchar(c2); 
+	putchar(c3); 
+	putchar(c4); 
+	putchar(c5); 
+ 	putchar('\n'); 
+
+ 	return 0; 
+} 
