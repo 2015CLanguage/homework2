0ce1b847765f213c6fc2f531581ccca317c35078
2015CLanguage <buaa_2015_c@163.com>
2015-03-28 14:59:12 +0800

    Create 13005032_马志珍_3_6.c

---
+++ 13005032_马志珍_3_6.c
@@ -0,0 +1,18 @@
+Enter file contents here
+#include<stdio.h>
+int main()
+{
+  char c1,c2,c3,c4,c5,a1,a2,a3,a4,a5;
+  a1='C';
+  a2='h';
+  a3='i';
+  a4='n';
+  a5='a';
+  c1=a1+4;
+  c2=a2+4;
+  c3=a3+4;
+  c4=a4+4;
+  c5=a5+4;
+  printf("%c%c%c%c%c",c1,c2,c3,c4,c5);
+  return 0;
+}
